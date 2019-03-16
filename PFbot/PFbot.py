fb6013e87c30107e05bc5c80138bb43fec629038
bpelikan <bartoszpelikan@gmail.com>
2019-03-16 17:39:25 +0100

    Read account credential

--- PFbot/PFbot.py
+++ PFbot/PFbot.py
@@ -3,14 +3,45 @@
 
 #https://portal.pixelfederation.com/en/profile
 
-import os
+import os, sys, logging, winsound
 from selenium import webdriver
 
-profileURL = "https://portal.pixelfederation.com/en/profile"
 
-os.chdir('C:\\Users\\Barpel\\Documents\\PythonScripts\\PFbotAcc')
-currentPath = os.getcwd()
-print('Current path: %s' %currentPath)
+##################################################
+##                 FUNCTIONS                    ##
+##################################################
+def playErrorSound():
+    winsound.PlaySound('C:\\Windows\\media\\Windows Exclamation.wav', winsound.SND_FILENAME)
+
+def getAccountLoginData(logPath, fileName):
+    try:
+        filePath = os.path.join(logPath, fileName)
+        logging.debug('Funkcja: getAccountLoginData(%s)' %filePath)
+        file = open(filePath)
+        filedata = file.readlines()
+        for i in range(len(filedata)-1):
+            filedata[i] = filedata[i][:-1]
+        return filedata
+    except Exception as err:
+        logging.error('An exception happened during loading account login data: ' + str(err))
+        playErrorSound()
+        return ['','']
+    finally:
+        file.close()
+
+
+
+filesPath = 'C:\\Users\\Barpel\\Documents\\PythonScripts\\PFbotAcc'
+os.chdir(filesPath)
+if filesPath != os.getcwd():
+    sys.exit()
+print('Current path: %s' %os.getcwd())
+
+profileURL = "https://portal.pixelfederation.com/en/profile"
+accountLoginFileName = 'logpf.txt'
+
+myUserName, myUserPass = getAccountLoginData(filesPath, accountLoginFileName)
+
 
 print("open chrome")
 browser = webdriver.Chrome()
